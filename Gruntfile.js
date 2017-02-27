38f62af7671b4b39a16b7291e11fb0b9e43d1e03
emmaga <emma_0812@msn.cn>
2017-02-27 14:27:10 +0800

    修改：把ip配置文件从压缩文件中分离

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,6 @@ module.exports = function(grunt) {
                 files: [
                     {
                         src: [
-                            'src/modules/app.js',
                             'src/modules/controllers.js',
                             'src/modules/directives.js',
                             'src/modules/services.js',
